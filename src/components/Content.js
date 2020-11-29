5a3ba97f7187c892134b45c916d1fdc5f346c5f3
elina-naomi <elina-naomi@mail.ru>
2020-11-29 11:34:31 +0200

    Profile tab /profile includes form. Header & left bar - sticky. Scroll bar is always visible.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,38 +1,36 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import styles from "../css_modules/content.module.css";
 import {Route, Switch} from "react-router-dom";
-import {activitiesPage, foundPage, lostPage, profilePage} from "../utils/Constants";
+import {activitiesPage, profilePage} from "../utils/Constants";
 import ProfileComplex from "./ProfileComplex";
 import ContentTitle from "./ContentTitle";
 import ButtonOutlinedWhite from "./buttons/ButtonOutlinedWhite";
 import ButtonSaveChanges from "./buttons/ButtonSaveChanges";
-import ButtonLostPet from "./buttons/ButtonLostPet";
-import ButtonFoundPet from "./buttons/ButtonFoundPet";
 
 const Content = (props) => {
     return (
         <div className={`${props.colQuantity} bg-white ${styles.mainContent} `}>
 
+            {/*Отображение ContentTitle*/}
             <Route
-                path={[`/${profilePage}`,`/${activitiesPage}`]}
+                path={[`/${profilePage}`, `/${activitiesPage}`]}
                 exact component={ContentTitle}/>
-
-
 
 
             <Switch>
                 <Route
-                    path={[`/${profilePage}`,`/${activitiesPage}`]}
-                    exact component={ProfileComplex}/>
+                    path={[`/${profilePage}`, `/${activitiesPage}`]}
+                    exact component={ProfileComplex}>
+                </Route>
             </Switch>
 
+            {/*Отображение кнопок Save и Cancel*/}
             <Route path={`/${profilePage}`} exact>{
-                <div className='mt-4 mr-2 float-right'>
+                <div className='mt-4 mr-2 pb-2 float-right'>
                     <ButtonOutlinedWhite marginRight='mr-3'/>
                     <ButtonSaveChanges/>
                 </div>
             }</Route>
-
 
 
         </div>
